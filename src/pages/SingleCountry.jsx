d1ace33adab68504d93652008f7b837354beaff8
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-10-14 20:59:54 +0500

    updated SingleCountry.jsx

--- src/pages/SingleCountry.jsx
+++ src/pages/SingleCountry.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/styles/singleCountry.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { IoMdArrowBack } from "react-icons/io";
+import axios from "axios";
 
 const SingleCountry = () => {
+  const [countries, setCountries] = useState([]);
+  const [singleCountry, setSingleCountry] = useState();
+  const { cca2: countryCode } = useParams();
+
+  const getCountries = () => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setCountries(res.data));
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  useEffect(() => {
+    if (countries.length > 0 && countryCode) {
+      setSingleCountry(
+        countries.filter((country) => country.cca2 === countryCode)
+      );
+    }
+  }, [countries, countryCode]);
+
+  console.log(singleCountry);
+
   return (
     <main>
       <section>
